60bfc4b0a5eb54538231ae51172d8e10fcfc532b
ashishlodhi1234 <61156300+ashishlodhi1234@users.noreply.github.com>
2020-11-27 17:49:43 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ exports.run = async (client, message) => {
             footer: { text: 'HAPPY' },
             fields: [
                 { name: 'Bot', value: '`ping`' },
-                { name: 'Music', value: '`play`, `pause`, `resume`, `queue`, `clear-queue`, `shuffle`, `np`, `loop`, `volume`, `skip`, `stop`, `filter`, `w-filters`' },
+                { name: 'Music', value: '`play`, `pause`, `resume`, `queue`, `clear-queue`, `shuffle`, `np`, `loop`, `volume`, `skip`, `stop`, `filter`, `enable-filters`' },
                 { name: 'Filters', value: '`bassboost`, `8D`, `vaporwave`, `nightcore`, `phaser`, `tremolo`, `vibrato`, `reverse`, `treble`, `normalizer`, `surrounding`, `pulsator`, `subboost`, `karaoke`, `flanger`, `gate`, `haas`, `mcompand`' },
             ],
             timestamp: new Date(),
