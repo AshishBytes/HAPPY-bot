cd747b99cddbe55ea0781d39c949367316a98d52
ashishlodhi1234 <61156300+ashishlodhi1234@users.noreply.github.com>
2020-11-27 18:03:23 +0530

    Update trackStart.js

--- player-events/trackStart.js
+++ player-events/trackStart.js
@@ -1,5 +1,5 @@
 module.exports = (client, message, track) => {
 
-    message.channel.send(`${client.emotes.music} - Now playing ${track.title} into #${message.member.voice.channel.name} ...`);
+    message.channel.send(`${client.emotes.music} - Now playing ${track.title} into <#${message.member.voice.channel.client}> ...`);
 
 };
