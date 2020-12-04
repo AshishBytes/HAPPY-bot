4f587ab0e292f3a36b4cce4e95481dacffa6eceb
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-04 15:28:58 +0530

    Create vote.js

---
+++ commands/vote.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+exports.run = (client, message, args) =>{
+    let serverembed = new Discord.MessageEmbed()
+        .setColor("#00FFFF")
+        .addField(`Invite`, `[Invite Link](https://top.gg/bot/722790472279523339/vote)`, true)
+        .setFooter(`HAPPY`);
+
+    message.channel.send(serverembed);    
+
+}
