ccdbac425aa73a4ea61210841afda6d632128111
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-04 14:44:54 +0530

    Update dc.js

--- commands/dc.js
+++ commands/dc.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) =>{
     if(!message.member.voice.channel)
         return message.channel.send("You are not in a voice channel");
     else{
-        message.channel.send("Bye bye!");
+        message.channel.send("DONE");
         return message.guild.me.voice.channel.leave();
     }
 }
