bc71d2756394264b21543ec76ef07a2c9f54b6a5
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-04 15:39:31 +0530

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -2,9 +2,7 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) =>{
     let serverembed = new Discord.MessageEmbed()
         .setColor("#00FFFF")
-        .addField(`Invite`, `[Invite Link](https://top.gg/bot/722790472279523339/invite)`, false)
-	      .addField(`Vote`, `[Vote Link](https://top.gg/bot/722790472279523339/vote)`, false)
-	      .addField(`Server`, `[Server Link](currently nothing)`, true)
+        .addField(`Server`, `[Server Link](currently nothing)`, true)
         .setFooter(`HAPPY`);
 
     message.channel.send(serverembed);    
