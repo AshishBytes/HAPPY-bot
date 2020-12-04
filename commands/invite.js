576642436196982359d983ba99b77642bc37c812
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-04 15:24:47 +0530

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+exports.run = (client, message, args) =>{
+    let serverembed = new Discord.MessageEmbed()
+        .setColor("#00FFFF")
+        .setAuthor(`RaptorSA`, client.user.displayAvatarURL)
+        .addField(`Invite`, `[Invite Link](https://top.gg/bot/722790472279523339/invite)`, true)
+        .setFooter(`HAPPY`);
+
+    message.channel.send(serverembed);    
+
+}
