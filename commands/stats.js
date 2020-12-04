050a9a88a6b46c1be9ca41aeec15cc4468c6d182
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-04 16:14:17 +0530

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
     let serverembed = new Discord.MessageEmbed()
         .setColor("#00FFFF")
-        .addField(`Memory`, `${memory}`, true)
+        .addField(`Memory`, `${memory}`, false)
         .addField(`Servers`, `${servers}`, true)
         .addField(`Users`, `${users}`, true)
         .addField(`Invite`, `[Invite Me](https://top.gg/bot/722790472279523339/invite)`, true)
-		 addField(`Vote`, `[Vote Me](https://top.gg/bot/722790472279523339/invite)`, true)
+        .addField(`Vote`, `[Vote Me](https://top.gg/bot/722790472279523339/invite)`, true)
         .setFooter(`Uptime: ${uptime}`);
 
     message.channel.send(serverembed);    
