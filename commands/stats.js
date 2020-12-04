91ead43ace2f07d399ac5ec780a26a7024e3744e
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-04 16:15:24 +0530

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
     let serverembed = new Discord.MessageEmbed()
         .setColor("#00FFFF")
-        .addField(`Memory`, `${memory}`, false)
         .addField(`Servers`, `${servers}`, true)
         .addField(`Users`, `${users}`, true)
         .addField(`Invite`, `[Invite Me](https://top.gg/bot/722790472279523339/invite)`, true)
