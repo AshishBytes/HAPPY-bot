7772a597318b44eb6bb551c299e22e9b7bce810b
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-07 19:30:39 +0530

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) =>{
     let serverembed = new Discord.MessageEmbed()
         .setColor("#00FFFF")
+        .setThumbnail(client.user.avatarURL({ format: 'png', dynamic: true, size: 2048 }))
+        .setURL(client.user.avatarURL({ format: 'png', dynamic: true, size: 2048 }))
         .addField(`Vote`, `[Vote](https://top.gg/bot/722790472279523339/vote)`, true)
         .setFooter(`HAPPY`);
 
