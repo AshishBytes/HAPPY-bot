68e40d7fa8785205a62d0ff972c1fa2c027ce265
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-07 19:30:05 +0530

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) =>{
     let serverembed = new Discord.MessageEmbed()
         .setColor("#00FFFF")
+        .setThumbnail(client.user.avatarURL({ format: 'png', dynamic: true, size: 2048 }))
+        .setURL(client.user.avatarURL({ format: 'png', dynamic: true, size: 2048 }))
         .addField(`Invite`, `[Invite Link](https://top.gg/bot/722790472279523339/invite)`, true)
         .setFooter(`HAPPY`);
 
