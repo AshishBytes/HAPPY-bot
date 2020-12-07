34bb785fd5a4b91cce06b3e70bdf3c26566f371f
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-07 19:31:09 +0530

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -2,9 +2,11 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) =>{
     let serverembed = new Discord.MessageEmbed()
         .setColor("#00FFFF")
+        .setThumbnail(client.user.avatarURL({ format: 'png', dynamic: true, size: 2048 }))
+        .setURL(client.user.avatarURL({ format: 'png', dynamic: true, size: 2048 }))
         .addField(`Invite`, `[Invite Link](https://top.gg/bot/722790472279523339/invite)`, true)
-	    .addField(`Vote`, `[Vote Link](https://top.gg/bot/722790472279523339/vote)`, true)
-	    .addField(`Server`, `[Server Link](https://discord.gg/qZuGW4f6yv)`, true)
+	.addField(`Vote`, `[Vote Link](https://top.gg/bot/722790472279523339/vote)`, true)
+	.addField(`Server`, `[Server Link](https://discord.gg/qZuGW4f6yv)`, true)
         .setFooter(`HAPPY`);
 
     message.channel.send(serverembed);    
