a4dabb9bd5100f81d101b1694d3bc16361c3af88
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-07 19:28:08 +0530

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -4,6 +4,7 @@ exports.run = (client, message, args) =>{
         .setColor("#00FFFF")
         .setThumbnail(client.user.avatarURL({ format: 'png', dynamic: true, size: 2048 }))
         .setURL(client.user.avatarURL({ format: 'png', dynamic: true, size: 2048 }))
+
         .addField(`Server`, `[Server Link](https://discord.gg/qZuGW4f6yv)`, true)
         .setFooter(`HAPPY`);
 
