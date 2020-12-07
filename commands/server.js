d1e35b2c217bb55e2d48dd65562f2f8596d673e3
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-07 19:26:38 +0530

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) =>{
     let serverembed = new Discord.MessageEmbed()
         .setColor("#00FFFF")
+        .setThumbnail(client.user.avatarURL({ format: 'png', dynamic: true, size: 2048 }))
+        .setURL(client.user.avatarURL({ format: 'png', dynamic: true, size: 2048 }))
         .addField(`Server`, `[Server Link](https://discord.gg/qZuGW4f6yv)`, true)
         .setFooter(`HAPPY`);
 
