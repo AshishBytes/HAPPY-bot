3de911d7ee2819a72199a331e401c27b567decc2
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-07 14:52:31 +0530

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) =>{
     let serverembed = new Discord.MessageEmbed()
         .setColor("#00FFFF")
-        .addField(`Server`, `[Server Link](currently nothing)`, true)
+        .addField(`Server`, `[Server Link](https://discord.gg/qZuGW4f6yv)`, true)
         .setFooter(`HAPPY`);
 
     message.channel.send(serverembed);    
