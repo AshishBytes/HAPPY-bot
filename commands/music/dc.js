c468536164bec80955831e6c2fed6190b8b66115
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-10 12:01:04 +0530

    Delete dc.js

--- commands/music/dc.js
+++
@@ -1,8 +0,0 @@
-exports.run = (client, message, args) =>{
-    if(!message.member.voice.channel)
-        return message.channel.send("You are not in a voice channel");
-    else{
-        message.channel.send("DONE");
-        return message.guild.me.voice.channel.leave();
-    }
-}
