35f909e70b411631f97fc8d3f30d04b848c5c5a2
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-10 08:24:48 +0530

    Update dm.js

--- commands/test/dm.js
+++ commands/test/dm.js
@@ -1,13 +1,124 @@
-const Discord = require("discord.js");
+	const Discord = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
-  
-if(message.author.id != "707503577731432529") return message.channel.send("<@707503577731432529>")
-  message.channel.send(`Serving ${client.guilds.size} servers`)
-  message.channel.send(client.guilds.map(g=>g.name).join('\n'))
-  }
+class ServersList extends Command {
 
+	constructor (client) {
+		super(client, {
+			name: "dm",
+			dirname: __dirname,
+			enabled: true,
+			guildOnly: false,
+			aliases: [ "dmowner" ],
+			memberPermissions: [],
+			botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
+			nsfw: false,
+			ownerOnly: true,
+			cooldown: 5000
+		});
+	}
 
-module.exports.help = {
-  name: "dm"
+	async run (message, args, data) {
+        
+		await message.delete();
+
+		let i0 = 0;
+		let i1 = 10;
+		let page = 1;
+
+		let description = 
+        `${message.translate("common:SERVERS")}: ${this.client.guilds.cache.size}\n\n`+
+		this.client.guilds.cache.sort((a,b) => b.memberCount-a.memberCount).map((r) => r)
+			.map((r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} ${message.translate("common:MEMBERS").toLowerCase()}`)
+			.slice(0, 10)
+			.join("\n");
+
+		const embed = new Discord.MessageEmbed()
+			.setAuthor(message.author.tag, message.author.displayAvatarURL())
+			.setColor(data.config.embed.color)
+			.setFooter(this.client.user.username)
+			.setTitle(`${message.translate("common:PAGE")}: ${page}/${Math.ceil(this.client.guilds.cache.size/10)}`)
+			.setDescription(description);
+
+		const msg = await message.channel.send(embed);
+        
+		await msg.react("⬅");
+		await msg.react("➡");
+		await msg.react("❌");
+
+		const collector = msg.createReactionCollector((reaction, user) => user.id === message.author.id);
+
+		collector.on("collect", async(reaction) => {
+
+			if(reaction._emoji.name === "⬅") {
+
+				// Updates variables
+				i0 = i0-10;
+				i1 = i1-10;
+				page = page-1;
+                
+				// if there is no guild to display, delete the message
+				if(i0 < 0){
+					return msg.delete();
+				}
+				if(!i0 || !i1){
+					return msg.delete();
+				}
+                
+				description = `${message.translate("common:SERVERS")}: ${this.client.guilds.cache.size}\n\n`+
+				this.client.guilds.cache.sort((a,b) => b.memberCount-a.memberCount).map((r) => r)
+					.map((r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} ${message.translate("common:MEMBERS")}`)
+					.slice(i0, i1)
+					.join("\n");
+
+				// Update the embed with new informations
+				embed.setTitle(`${message.translate("common:PAGE")}: ${page}/${Math.round(this.client.guilds.cache.size/10)}`)
+					.setDescription(description);
+            
+				// Edit the message 
+				msg.edit(embed);
+            
+			}
+
+			if(reaction._emoji.name === "➡"){
+
+				// Updates variables
+				i0 = i0+10;
+				i1 = i1+10;
+				page = page+1;
+
+				// if there is no guild to display, delete the message
+				if(i1 > this.client.guilds.cache.size + 10){
+					return msg.delete();
+				}
+				if(!i0 || !i1){
+					return msg.delete();
+				}
+
+				description = `${message.translate("common:SERVERS")}: ${this.client.guilds.cache.size}\n\n`+
+				this.client.guilds.cache.sort((a,b) => b.memberCount-a.memberCount).map((r) => r)
+					.map((r, i) => `**${i + 1}** - ${r.name} | ${r.memberCount} ${message.translate("common:MEMBERS").toLowerCase()}`)
+					.slice(i0, i1)
+					.join("\n");
+
+				// Update the embed with new informations
+				embed.setTitle(`${message.translate("common:PAGE")}: ${page}/${Math.round(this.client.guilds.cache.size/10)}`)
+					.setDescription(description);
+            
+				// Edit the message 
+				msg.edit(embed);
+
+			}
+
+			if(reaction._emoji.name === "❌"){
+				return msg.delete(); 
+			}
+
+			// Remove the reaction when the user react to the message
+			await reaction.users.remove(message.author.id);
+
+		});
+	}
+
 }
+
+module.exports = ServersList;
