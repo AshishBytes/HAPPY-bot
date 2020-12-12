7808c7dee503ac283ff5d251d66873ff6aca6a08
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-12 16:37:22 +0530

    Update stop.js

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -1,17 +1,12 @@
-module.exports = {
-    name: 'stop',
-    aliases: ['dc', 'leave', 'left', 'bhak'],
-    category: 'Music',
-    utilisation: '{prefix}stop',
+exports.run = async (client, message) => {
 
-    execute(client, message) {
-        if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
+    if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 
-        if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - No music currently playing !`);
+    if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - No music currently playing !`);
 
-        client.player.setRepeatMode(message, false);
-        client.player.stop(message);
+    client.player.setRepeatMode(message, false);
+    client.player.stop(message);
 
-        message.channel.send(`${client.emotes.success} - Music **stopped** into this server !`);
-    },
+    message.channel.send(`${client.emotes.success} - Music **stopped** into this server !`);
+
 };
