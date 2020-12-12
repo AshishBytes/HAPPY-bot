eb429aef2c097d35b0038ac1d62d5f498604a9a2
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-12 18:34:55 +0530

    Update credits.js

--- commands/info/credits.js
+++ commands/info/credits.js
@@ -8,10 +8,10 @@ exports.run = (client, message, args) =>{
         .setTimestamp()
         .addField("HAPPY", "Show the bot's creadits.")
         .addField("-------------------------------------------------------------------------------","----------------------------------------------------------------------------")
-        .setField("BIG THANKS FOR MUSIC");
-	.addField(`GITHUB`, `[LINK](https://github.com/ZerioDev/Music-bot)`, true):
-	.addField(`SERVER`, `[LINK](https://discord.gg/DKXWFfCAEE)`, true):
-	.setFooter("HAPPY");
+        .setField("BIG THANKS FOR MUSIC")
+	.addField(`GITHUB`, `[LINK](https://github.com/ZerioDev/Music-bot)`, true)
+	.addField(`SERVER`, `[LINK](https://discord.gg/DKXWFfCAEE)`, true)
+	.setFooter("HAPPY")
 
     message.channel.send(serverembed);    
 
