3ec743ffdefab73b3b9dbbcb33a128342bb82909
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-12 16:17:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,53 +4,52 @@ const discord = require('discord.js');
 const client = new discord.Client({ disableMentions: 'everyone' });
 
 const { Player } = require('discord-player');
-const Player = new Player(client, {
+const player = new Player(client, {
     leaveOnEnd: false,
     leaveOnStop: true,
     leaveOnEmpty: false,
     quality: 'high',
 });
-client.player = new Player(client);
+const player = new Player(client);
+client.player = player;
 client.config = require('./config/bot.json');
 client.emotes = require('./config/emojis.json');
 client.filters = require('./config/filters.json');
 client.commands = new discord.Collection();
+		
+const init = async () => {
 
-const moderation = fs.readdirSync('./commands/moderation').filter(file => file.endsWith('.js'));
-const info = fs.readdirSync('./commands/info').filter(file => file.endsWith('.js'));
-const music = fs.readdirSync('./commands/music').filter(file => file.endsWith('.js'));
+	// Search for all commands
+	const directories = await readdir("./commands/");
+	client.logger.log(`Loading a total of ${directories.length} categories.`, "log");
+	directories.forEach(async (dir) => {
+		const commands = await readdir("./commands/"+dir+"/");
+		commands.filter((cmd) => cmd.split(".").pop() === "js").forEach((cmd) => {
+			const response = client.loadCommand("./commands/"+dir, cmd);
+			if(response){
+				client.logger.log(response, "error");
+			}
+		});
+	});
+    
+fs.readdir('./events/', (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        const event = require(`./events/${file}`);
+        let eventName = file.split(".")[0];
+        console.log(`Loading event ${eventName}`);
+        client.on(eventName, event.bind(null, client));
+    });
+});
 
-for (const file of moderation) {
-    console.log(`Loading command ${file}`);
-    const command = require(`./commands/moderation/${file}`);
-    client.commands.set(command.name.toLowerCase(), command);
-};
-
-for (const file of info) {
-    console.log(`Loading command ${file}`);
-    const command = require(`./commands/info/${file}`);
-    client.commands.set(command.name.toLowerCase(), command);
-};
-
-for (const file of music) {
-    console.log(`Loading command ${file}`);
-    const command = require(`./commands/music/${file}`);
-    client.commands.set(command.name.toLowerCase(), command);
-};
-
-const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-const player = fs.readdirSync('./player').filter(file => file.endsWith('.js'));
-
-for (const file of events) {
-    console.log(`Loading discord.js event ${file}`);
-    const event = require(`./events/${file}`);
-    client.on(file.split(".")[0], event.bind(null, client));
-};
-
-for (const file of player) {
-    console.log(`Loading discord-player event ${file}`);
-    const event = require(`./player/${file}`);
-    client.player.on(file.split(".")[0], event.bind(null, client));
-};
+fs.readdir('./player-events/', (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        const event = require(`./player-events/${file}`);
+        let eventName = file.split(".")[0];
+        console.log(`Loading player event ${eventName}`);
+        client.player.on(eventName, event.bind(null, client));
+    });
+});
 
 client.login(client.config.token_bot);
