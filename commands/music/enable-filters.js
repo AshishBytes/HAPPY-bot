ae732bc5df2d45546ad11e25daa2d8418dc07346
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-12 16:38:00 +0530

    Update enable-filters.js

--- commands/music/enable-filters.js
+++ commands/music/enable-filters.js
@@ -1,35 +1,30 @@
-module.exports = {
-    name: 'enable-filters',
-    aliases: ['filters'],
-    category: 'Music',
-    utilisation: '{prefix}enable-filters',
+exports.run = async (client, message) => {
 
-    execute(client, message) {
-        if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
+    if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 
-        if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - No music currently playing !`);
+    if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - No music currently playing !`);
 
-        const disabledEmoji = client.emotes.error;
-        const enabledEmoji = client.emotes.success;
+    const disabledEmoji = client.emotes.error;
+    const enabledEmoji = client.emotes.success;
 
-        const filtersStatuses = [[], []];
+    const filtersStatuses = [[], []];
 
-        Object.keys(client.filters).forEach((filterName) => {
-            const array = filtersStatuses[0].length > filtersStatuses[1].length ? filtersStatuses[1] : filtersStatuses[0];
-            array.push(client.filters[filterName] + " : " + (client.player.getQueue(message).filters[filterName] ? enabledEmoji : disabledEmoji));
-        });
+    Object.keys(client.filters).forEach((filterName) => {
+        const array = filtersStatuses[0].length > filtersStatuses[1].length ? filtersStatuses[1] : filtersStatuses[0];
+        array.push(client.filters[filterName] + " : " + (client.player.getQueue(message).filters[filterName] ? enabledEmoji : disabledEmoji));
+    });
 
-        message.channel.send({
-            embed: {
-                color: 'AQUA',
-                footer: { text: 'HAPPY' },
-                fields: [
-                    { name: 'Filters', value: filtersStatuses[0].join('\n'), inline: true },
-                    { name: '** **', value: filtersStatuses[1].join('\n'), inline: true },
-                ],
-                timestamp: new Date(),
-                description: `List of all filters enabled or disabled.\nUse \`${client.config.prefix}filter\` to add a filter to a song.`,
-            },
-        });
-    },
+    message.channel.send({
+        embed: {
+            color: 'AQUA',
+            footer: { text: 'HAPPY' },
+            fields: [
+                { name: 'Filters', value: filtersStatuses[0].join('\n'), inline: true },
+                { name: '** **', value: filtersStatuses[1].join('\n'), inline: true },
+            ],
+            timestamp: new Date(),
+            description: `List of all filters enabled or disabled.\nUse \`${client.config.prefix}filter\` to add a filter to a song.`,
+        },
+    });
+
 };
