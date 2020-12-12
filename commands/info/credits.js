e04d53cdcf7db45fb6b78d10069cce300a2ec374
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2020-12-12 18:16:25 +0530

    Create credits.js

---
+++ commands/info/credits.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const osutils = require('os-utils');
+exports.run = (client, message, args) =>{
+    let serverembed = new Discord.MessageEmbed()
+        .setColor("#00FFFF")
+        .setThumbnail(client.user.avatarURL({ format: 'png', dynamic: true, size: 2048 }))
+        .setURL(client.user.avatarURL({ format: 'png', dynamic: true, size: 2048 }))
+        .setTimestamp()
+        .addField("HAPPY", "Show the bot's creadits.")
+        .addField("-------------------------------------------------------------------------------","----------------------------------------------------------------------------")
+        .setField("BIG THANKS FOR MUSIC");
+	    .addField(`GITHUB`, `[LINK](https://github.com/ZerioDev/Music-bot)`, true)
+	    .addField(`SERVER`, `[LINK](https://discord.gg/DKXWFfCAEE)`, true)
+	    .setFooter("HAPPY");
+
+    message.channel.send(serverembed);    
+
+}
