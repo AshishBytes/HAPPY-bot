3989611188343d16497711ee0953c9e53e4777d0
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-02-15 17:41:44 +0530

    Update np.js

--- commands/music/np.js
+++ commands/music/np.js
@@ -14,7 +14,7 @@ exports.run = async (client, message) => {
     message.channel.send({
         embed: {
             color: 'AQUA',
-            author: '♪Now playing♪', value: track.title},
+            author: '♪Now playing♪': track.title},
             footer: { text: 'HAPPY' },
             fields: [
                 { name: 'Channel', value: track.author, inline: true },
