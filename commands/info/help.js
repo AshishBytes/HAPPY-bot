e822ff1ea299f772e22b24173e75acf442d618fb
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-02-16 22:31:18 +0530

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -9,7 +9,7 @@ exports.run = async (client, message) => {
                 { name: 'Music', value: '`play`,`join`,`pause`, `resume`, `queue`, `clear-queue/cq`, `shuffle`, `np`, `loop`, `volume`, `skip`, `stop/left/leave`, `filter`, `filters`, `enable-filters`' },
                 { name: 'Filters', value: '`bassboost`, `8D`, `vaporwave`, `nightcore`, `phaser`, `tremolo`, `vibrato`, `reverse`, `treble`, `normalizer`, `surrounding`, `pulsator`, `subboost`, `karaoke`, `flanger`, `gate`, `haas`, `mcompand`' },
                 { name: 'Moderation', value: '`purge`,`clear`,`ban`,`kick`' },
-	        { name: 'Info', value: '`help`,`info`,`serverinfo`,`userinfo`,`uptime`' },
+	        { name: 'Info', value: '`help`,`info`,`serverinfo`,`uptime`' },
 		{ name: 'Support', value: '`support`,`server`,`invite`' },
 		{ name: 'Other', value: '`createinvite`,`ping`' },
             ],
