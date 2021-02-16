9919ddba8db80ae9d0b95917a6fba40d513b0e1b
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-02-16 22:15:54 +0530

    Update support.js

--- commands/support/support.js
+++ commands/support/support.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) =>{
         .setColor("#00FFFF")
         .setThumbnail(client.user.avatarURL({ format: 'png', dynamic: true, size: 2048 }))
         .setURL(client.user.avatarURL({ format: 'png', dynamic: true, size: 2048 }))
-        .addField(`Invite`, `[Invite Link](https://top.gg/bot/722790472279523339/invite)`, true)
+        .addField(`Invite`, `[Invite Link](https://discord.com/api/oauth2/authorize?client_id=810825174990454794&permissions=37092672&scope=bot)`, true)
 	.addField(`Vote`, `[Vote Link](https://top.gg/bot/722790472279523339/vote)`, true)
 	.addField(`Server`, `[Server Link](https://discord.gg/qZuGW4f6yv)`, true)
         .setFooter(`HAPPY`);
