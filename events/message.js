a44ec08f3038e57d1da635eb3b07fa29b31fca59
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-02-17 22:05:37 +0530

    Update message.js

--- events/message.js
+++ events/message.js
@@ -4,11 +4,7 @@ module.exports = (client, message) => {
 
     const prefix = client.config.prefix;
 
-    if (message.content.indexOf(prefix) !== 0) return;
-      //information message when the bot has been tagged
-  if(message.content.includes(client.user.id)) {
-    message.reply(new Discord.MessageEmbed().setColor("#F0EAD6").setAuthor(`${message.author.username}, My Prefix is ${prefix}, to get started; type ${prefix}help`, message.author.displayAvatarURL({dynamic:true})));
-  } 
+    if (message.content.indexOf(prefix) !== 0) return; 
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
