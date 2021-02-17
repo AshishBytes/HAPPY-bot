6cd94bd956849d13588ab1c161972c6b38dc92cf
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-02-17 22:02:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -141,11 +141,4 @@ fs.readdir('./commands/test/', (err, files) => {
         client.commands.set(commandName, props);
     });
 });
-
-                    /////////////////////////////////
-
-client.on('message', async message => {
-if(message.content.match(new RegExp(`${client.user.id}`))) 
-return message.channel.send("For more information type '~help'")
-})
 client.login(client.config.token_bot);
