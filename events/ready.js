a1c41c99187a674e90c713011e455f7e43d0fc38
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-02-18 21:42:22 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
 
     console.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} users`);
-    new Client({ presence: { status: "idle" } });
+    client.user.setStatus('idle');
     client.user.setActivity("~help", { type: "LISTENING" });
 };
