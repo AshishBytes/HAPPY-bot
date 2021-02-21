e99d269a52db4c82b4c6f66ae04d40c677b945e6
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-02-21 22:30:07 +0530

    Update createinvite.js

--- commands/other/createinvite.js
+++ commands/other/createinvite.js
@@ -3,12 +3,12 @@ exports.run = (client, message, args) => {
 
     try{
         message.guild.channels.cache.get(setChannelID[1]).createInvite().then(invite =>
-            message.channel.send("The channel invite has been created: \n" + invite.url)
+            message.inlineReply("The channel invite has been created: \n" + invite.url)
         );
     }
 
     catch(error){
         console.error(`I could not create the invite for the channel: ${error}`);
-        message.channel.send(`You have to paste a correct channel ID!`);
+        message.inlineReply(`You have to paste a correct channel ID!`);
     }
 }
