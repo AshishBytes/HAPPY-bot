f6db3dab999a9e5b87d8a6f90e8525b38e3d92b5
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-02-21 22:35:44 +0530

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -3,13 +3,13 @@ exports.run = async (client, message, args) =>{
 
   // get the delete count, as an actual number.
   if(!message.member.hasPermission("MANAGE_MESSAGES")){
-    message.channel.send("You don't have the permissions to use this command!");
+    message.inlineReply("You don't have the permissions to use this command!");
   }
   
   else{        
     // Ooooh nice, combined conditions. <3
     if(!deleteCount || deleteCount < 2 || deleteCount > 100){
-      return message.channel.send("Please provide a number between 2 and 100 for the number of messages to clear");
+      return message.inlineReply("Please provide a number between 2 and 100 for the number of messages to clear");
     }
     
     await message.channel.bulkDelete(deleteCount).catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
