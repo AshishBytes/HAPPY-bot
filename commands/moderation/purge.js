6fe4a77717839f61e153f449565592fc7ec47f31
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-02-21 21:47:12 +0530

    Update purge.js

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -1,43 +1,37 @@
-exports.run = async (client, message, args) =>{
-    if (!message.guild.available) return this.client.logger.info(`Guild "${message.guild.name}" (${message.guild.id}) is unavailable.`);
-    if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`I cannot run this command as I have insufficient permissions to do so. Please ensure I have the \"Manage Messages\" permission.`);
-
-    const user = message.mentions.users.first();
-    const amount = parseInt(message.content.split(" ")[1]) ? parseInt(message.content.split(" ")[1]) : parseInt(message.content.split(" ")[2]);
-
-    if (!amount && !user) return message.reply('Command Usage: `clear [@USER_MENTION] <Number>`');
-    if (amount < 2 || amount > 99) return message.channel.send("You've provided an invalid number of messages to delete. Please ensure it's between 2 and 99 (inclusive).");
-
-    let messages = await message.channel.messages.fetch({ limit: amount });
-
-    if (user) {
-      const filterBy = user ? user.id : this.client.user.id;
-      messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
-
-      this.client.emit("messageDeleteBulk", messages);
-      for (const msg of messages) msg.channel.messages.delete(msg.id);
-      
-      message.channel.bulkDelete(messages)
-      .then(() => {
-        message.channel.send(`☑️ | **${amount}** messages were cleared.`);
-      })
-      .catch(error => {
-        this.client.logger.error(error);
-        message.channel.send(`An error occurred:\n\```${error.message}\````);
-      });
-    } else {
-      this.client.emit("messageDeleteBulk", messages);
-      for (const msg of messages.values()) msg.channel.messages.delete(msg.id);
-      
-      message.channel.bulkDelete(messages)
-      .then(() => {
-        message.channel.send(`☑️ | **${amount}** messages were cleared.`);
-      })
-      .catch(error => {
-        if (error.message === "You can only bulk delete messages that are under 14 days old.") return message.channel.send(error.message);
-        this.client.logger.error(error);
-        return message.channel.send(`An error occurred:\n\```${error.message}\````);
-      });
-    }
+exports.run = async (client, message, args) => {
+  // Parse args
+  let deleteCount = parseInt(args[0], 10)
+  // Check if Delete Count is bigger than discord can handle
+  if (deleteCount > 100) {
+    // Delete only 100 messages
+    deleteCount = 100
+    // Notify user about this
+    message.channel.send('Can handle only 100 messages. Deleting 100 messages').then(e => setTimeout(() => e.delete(), 2000))
   }
-
+  // Delete only one message if not specified how many
+  if (Number.isNaN(deleteCount)) deleteCount = 1
+  // console.log([deleteCount, typeof (deleteCount)])
+  try {
+    // Delete user message
+    await message.delete()
+    // Fetch messages
+    const fetched = await message.channel.messages.fetch({ limit: deleteCount })
+    // Delete messages
+    await message.channel.bulkDelete(fetched)
+  } catch (error) {
+    // Handle error
+    message.reply(`Couldn't delete messages because of: ${ error }`)
+    // Delete error message
+      .then(el => setTimeout(() => el.delete(), 5000))
+    // Exit function
+    return 0
+  }
+  // Notify if success
+  message.channel.send(`Successfully deleted ${ deleteCount } ${ deleteCount === 1 ? 'message' : 'messages' }.`).then(el => setTimeout(() => el.delete(), 3000))
+  return 0
+}
+exports.help = {
+  name: '`purge`',
+  description: 'Delete messages',
+  usage: 'purge `nubmer of messages to delete (default: 1)`'
+}
