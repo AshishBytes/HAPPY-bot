ab2988e7441a3c045570df11e3cb9f7b982f6e2a
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-02-21 22:35:13 +0530

    Update purge.js

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -6,7 +6,7 @@ exports.run = async (client, message, args) => {
     // Delete only 100 messages
     deleteCount = 100
     // Notify user about this
-    message.channel.send('Can handle only 100 messages. Deleting 100 messages').then(e => setTimeout(() => e.delete(), 2000))
+    message.inlineReply('Can handle only 100 messages. Deleting 100 messages').then(e => setTimeout(() => e.delete(), 2000))
   }
   // Delete only one message if not specified how many
   if (Number.isNaN(deleteCount)) deleteCount = 1
@@ -20,14 +20,14 @@ exports.run = async (client, message, args) => {
     await message.channel.bulkDelete(fetched)
   } catch (error) {
     // Handle error
-    message.reply(`Couldn't delete messages because of: ${ error }`)
+    message.inlineReply(`Couldn't delete messages because of: ${ error }`)
     // Delete error message
       .then(el => setTimeout(() => el.delete(), 5000))
     // Exit function
     return 0
   }
   // Notify if success
-  message.channel.send(`Successfully deleted ${ deleteCount } ${ deleteCount === 1 ? 'message' : 'messages' }.`).then(el => setTimeout(() => el.delete(), 3000))
+  message.inlineReply(`Successfully deleted ${ deleteCount } ${ deleteCount === 1 ? 'message' : 'messages' }.`).then(el => setTimeout(() => el.delete(), 3000))
   return 0
 }
 exports.help = {
