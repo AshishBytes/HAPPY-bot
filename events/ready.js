4ab08fdc094e5b4b316f3430164655803473fb90
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-02-28 23:15:40 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ module.exports = async (client) => {
     console.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} users`);
     client.on("ready", () => {
     client.user.setPresence({
-        game: { 
+        activity: { 
             name: '~help',
             type: 'LISTENING'
         },
