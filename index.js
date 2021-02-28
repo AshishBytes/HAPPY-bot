2eb180124546e8da078c471ebe976324c38c0fef
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-02-28 18:47:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -66,10 +66,10 @@ fs.readdir('./commands/', (err, files) => {
 });
                     /////////////////////////////////
 
-  fs.readdir("./events/giveaways/", (err, files) => {
+  fs.readdir("./commands/giveaways/", (err, files) => {
     if (err) console.log(err);
     files.forEach((file) => {
-      let event = require(`../events/giveaways/${file}`);
+      let event = require(`../commands/giveaways/${file}`);
       console.log("loaded Event: " + file);
       let eventName = file.split(".")[0];
       client.giveawaysManager.on(eventName, event.bind(null, client));
