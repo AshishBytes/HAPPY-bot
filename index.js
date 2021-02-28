5a5d0e99a984de0ab1c1c63d372f1ef83be91025
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-02-28 19:24:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const Enmap = require("enmap");
 const discord = require('discord.js');
 const client = new discord.Client({ disableMentions: 'everyone' });
 const { Player } = require('discord-player');
@@ -154,4 +155,68 @@ client.on('message', async message => {
 if(message.content.match(new RegExp(`${client.user.id}`))) 
 return message.inlineReply("My Prefix is ~, to get started; type '~help'")
 })
+                    /////////////////////////////////
+if (!db.get("giveaways")) db.set("giveaways", []);
+
+const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
+  // This function is called when the manager needs to get all the giveaway stored in the database.
+  async getAllGiveaways() {
+    // Get all the giveaway in the database
+    return db.get("giveaways");
+  }
+
+  // This function is called when a giveaway needs to be saved in the database (when a giveaway is created or when a giveaway is edited).
+  async saveGiveaway(messageID, giveawayData) {
+    // Add the new one
+    db.push("giveaways", giveawayData);
+    // Don't forget to return something!
+    return true;
+  }
+
+  async editGiveaway(messageID, giveawayData) {
+    // Gets all the current giveaways
+    const giveaways = db.get("giveaways");
+    // Remove the old giveaway from the current giveaways ID
+    const newGiveawaysArray = giveaways.filter(
+      (giveaway) => giveaway.messageID !== messageID
+    );
+    // Push the new giveaway to the array
+    newGiveawaysArray.push(giveawayData);
+    // Save the updated array
+    db.set("giveaways", newGiveawaysArray);
+    // Don't forget to return something!
+    return true;
+  }
+
+  // This function is called when a giveaway needs to be deleted from the database.
+  async deleteGiveaway(messageID) {
+    // Remove the giveaway from the array
+    const newGiveawaysArray = db
+      .get("giveaways")
+      .filter((giveaway) => giveaway.messageID !== messageID);
+    // Save the updated array
+    db.set("giveaways", newGiveawaysArray);
+    // Don't forget to return something!
+    return true;
+  }
+};
+
+// Create a new instance of your new class
+const manager = new GiveawayManagerWithOwnDatabase(client, {
+  storage: false, // Important - use false instead of a storage path
+  updateCountdownEvery: 10000,
+  endedGiveawaysLifetime: 30000,
+  hasGuildMembersIntent: false,
+  default: {
+    botsCanWin: false,
+    exemptPermissions: ["MANAGE_MESSAGES", "ADMINISTRATOR"],
+    embedColor: "#ff6969",
+    embedColorEnd: "#505050",
+    reaction: "🎉",
+  },
+});
+// We now have a giveawaysManager property to access the manager everywhere!
+client.giveawaysManager = manager;
+// We now have a client.giveawaysManager property to manage our giveaways!
+
 client.login(client.config.token_bot);
