a0c04ceb60afc2d4de8b83bf68d1872788330388
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-03-01 14:06:55 +0530

    Update uptime.js

--- commands/info/uptime.js
+++ commands/info/uptime.js
@@ -1,39 +1,90 @@
-const Discord = require('discord.js');
-let days = 0;
-let week = 0;
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+// let days = 0;
+// let week = 0;
+const os = require("os");
 
-exports.run = (client, message, args) =>{
-    let uptime = ``;
-    let totalSeconds = (client.uptime / 1000);
-    let hours = Math.floor(totalSeconds / 3600);
-    totalSeconds %= 3600;
-    let minutes = Math.floor(totalSeconds / 60);
-    let seconds = Math.floor(totalSeconds % 60);
+module.exports.run = async (client, message, args) => {
+  let nowtime = new Date().toLocaleString("en", { timeZone: "Asia/Hong_Kong" });
+  var milliseconds = parseInt((client.uptime % 1000) / 100),
+    seconds = parseInt((client.uptime / 1000) % 60),
+    minutes = parseInt((client.uptime / (1000 * 60)) % 60),
+    hours = parseInt((client.uptime / (1000 * 60 * 60)) % 24);
+  var days = parseInt((client.uptime / (1000 * 60 * 60 * 24)) % 60);
+  days = days < 10 ? "" + days : days;
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    if(hours > 23){
-        days = days + 1;
-        hours = 0;
-    }
+  var ut_sec = os.uptime();
+  var ut_min = ut_sec / 60;
+  var ut_hour = ut_min / 60;
+  var ut_day = ut_hour / 24;
 
-    if(days == 7){
-        days = 0;
-        week = week + 1;
-    }
+  ut_sec = Math.floor(ut_sec);
+  ut_min = Math.floor(ut_min);
+  ut_hour = Math.floor(ut_hour);
+  ut_day = Math.floor(ut_day);
 
-    if(week > 0){
-        uptime += `${week} week, `;
-    }
+  ut_day = ut_day % 24;
+  ut_hour = ut_hour % 60;
+  ut_min = ut_min % 60;
+  ut_sec = ut_sec % 60;
 
-    if(minutes > 60){
-        minutes = 0;
-    }
+  ut_day = ut_day < 10 ? "" + ut_day : ut_day;
+  ut_hour = ut_hour < 10 ? "0" + ut_hour : ut_hour;
+  ut_min = ut_min < 10 ? "0" + ut_min : ut_min;
+  ut_sec = ut_sec < 10 ? "0" + ut_sec : ut_sec;
+  let derweSA = client.mapss.get("uptimedate");
+  let serveddrembed = new Discord.MessageEmbed()
+    .setDescription("<a:loading:806686528549814344> Fetching Uptime...")
+    .setColor("RED");
 
-    uptime += `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+  message.channel.send(serveddrembed).then(async (message) => {
+    await fetch(
+      `https://api.hetrixtools.com/v1/f10ac71364c8b1aa149b4079fe8eafc9/uptime/report/483cfd9cb2dd306bf8c00917da1df827/`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        let numberas = data.Uptime_Stats.Total.Uptime.toLocaleString();
 
-    let serverembed = new Discord.MessageEmbed()
-        .setColor("#00FFFF")
-        .addField('Uptime', uptime);
+        let serverembedss = new Discord.MessageEmbed()
+          .setColor("#ffbbbb")
+          .addField(
+            "Bot Uptime",
+            days +
+              " Day, " +
+              hours +
+              " Hrs, " +
+              minutes +
+              " Min, " +
+              seconds +
+              " Sec"
+          )
+          .addField(
+            "Server/OS Uptime",
+            ut_day +
+              " Day, " +
+              ut_hour +
+              " Hrs, " +
+              ut_min +
+              " Min, " +
+              ut_sec +
+              " Sec"
+          )
+          .addField("Uptime Percentage", `${numberas}%`)
+          //	.addField("Now Time", nowtime)
+          //	.addField('Bootup Time', derweSA);;
+          .setFooter(`Last Launched at ${derweSA}`);
+        message.edit(serverembedss);
+      });
+  });
+};
 
-    message.inlineReply(serverembed);
-
-}
+module.exports.help = {
+  name: "uptime",
+  description: "This command is used for reporting bot's uptime",
+  usage: "d!uptime",
+  accessableby: "Member",
+  aliases: [],
+};
