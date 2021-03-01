500d137f76cf705a558c1aadfcfb1b5204b1b3a9
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-03-01 15:06:50 +0530

    Update snipe.js

--- commands/fun/snipe.js
+++ commands/fun/snipe.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 module.exports.run = async (bot, message, args) => {
     const msg = bot.snipes.get(message.channel.id)
     const embed = new Discord.MessageEmbed()
-    .setAuthor(msg.author, msg.member.user.displayAvatarURL())
+    .setAuthor(msg.author.id, msg.member.user.displayAvatarURL())
     .setDescription(msg.content)
     .setFooter('Get Sniped lol')
     .setTimestamp();
