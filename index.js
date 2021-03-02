c5e56a5c42039b10617effd12f0c0d21d73ef134
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-03-02 10:15:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Enmap = require("enmap");
 const db = require("quick.db");
 const discord = require('discord.js');
 const client = new discord.Client({ disableMentions: 'everyone' });
-const { Player } = require('discord-player');
+const { player } = require('discord-player');
 const { GiveawaysManager } = require("discord-giveaways");
 const cooldowns = new discord.Collection();
 require("./ExtendedMessage");
@@ -12,7 +12,7 @@ let nz_date_string = new Date().toLocaleString("en-US", {
 });
 client.mapss = new Map();
 client.mapss.set("uptimedate", nz_date_string);
-const player = new Player(client, {
+const player = new player(client, {
 	leaveOnEnd: true,
 	leaveOnStop: true,
 	leaveOnEmpty: false,
