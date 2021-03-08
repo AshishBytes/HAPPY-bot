50c7a1b18546175798b77696df6855455e68d5bc
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-03-08 09:26:46 +0530

    Update and rename f/vote.js to commands/support/vote.js

--- commands/support/vote.js
+++ commands/support/vote.js
@@ -4,8 +4,8 @@ exports.run = (client, message, args) =>{
         .setColor("#00FFFF")
         .setThumbnail(client.user.avatarURL({ format: 'png', dynamic: true, size: 2048 }))
         .setURL(client.user.avatarURL({ format: 'png', dynamic: true, size: 2048 }))
-        .addField(`Vote`, `[Vote](https://top.gg/bot/722790472279523339/vote)`, true)
-        .setFooter(`HAPPY`);
+        .addField(`Vote`, `[Vote](https://top.gg/bot/810825174990454794/vote)`, true)
+        .setFooter(`HAPPY | Please vote me for after every 12 hours`);
 
     message.channel.send(serverembed);    
 
