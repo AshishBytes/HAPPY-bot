7e25b603ebc106f4ced1a3cd88193b902a2e472d
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-03-08 09:33:51 +0530

    Create help2.js

---
+++ commands/info/help2.js
@@ -0,0 +1,98 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+	let helpArray = message.content.split(" ");
+	let helpArgs = helpArray.slice(1);
+
+	if (!helpArgs[0]) {
+		const embed = new Discord.MessageEmbed()
+			.setTitle("DraconianBot Help & Commands list")
+			.setColor("GREEN")
+			.setDescription(
+				"**prefix** `~`\nMore Info please visit: [Here](https://top.gg/bot/810825174990454794) and invite me to your server."
+			)
+			.addField("**📱Basic**", "`help`, `ping`, `vote`, `uptime`")
+			.addField(
+				"**⚙utility**",
+				"`aes256`, `avatar`, `channel`, `embed`, `roleinfo`, `reverse`, `setafk`, `snipe`, `stats`, `timer`, `translate`, `whois`, `weather`, `youtube`"
+			)
+			.addField(
+				"**🎃Fun**",
+				"`8ball`, `cat`, `deaes256`, `kiss`, `meme`, `ngif`, `pat`, `poke`, `smug`, `spank`, `thigh`, `tickle`"
+			)
+			.addField(
+				"**:tada:Giveaways**",
+				"`start-giveaway`, `reroll`, `end-giveaway`"
+			)
+			.addField(
+				"**:frame_photo:Image**",
+				"`circle`, `delete`, `gay`, `changemymind`, `trigger`, `clyde`"
+			)
+			.addField(
+				"**:musical_note:Music**",
+				"`play`, `stop`, `skip`, `queue`, `autoplay`, `loop`, `volume`, `pause`, `resume`"
+			)
+			.addField(
+				"**🛠️Moderation**",
+				"`addrole`, `ban`, `clear`, `clearwarn`, `createchannel`, `createemoji`, `kick`, `lockchannel`, `mute`, `rename`, `slowmode`, `unban`, `unlockchannel`, `unmute`, `warn`, `warnings`"
+			)
+			.addField(
+				"**:underage:NSFW**",
+				"`4knsfw`, `anal`, `ass`, `hentai`, `holo`, `pussy`, `porn`, `spank`, `urban`"
+			)
+			.addField("**:gear:Custom Function**", "`setprefix`")
+			.setFooter(
+				`HAPPY | This command requested by ${message.author.username}#${message.author.discriminator}`
+			);
+		message.channel.send({ embed });
+	}
+
+	if (helpArgs[0]) {
+		let command = helpArgs[0];
+
+		if (bot.commands.has(command)) {
+			command = bot.commands.get(command);
+			let alia = command.help.aliases;
+			if (command.help.aliases < 1) alia = "No aliases";
+
+			var embed = new Discord.MessageEmbed()
+				.setTitle(`**Command: ${command.help.name}**`)
+				.setDescription(
+					`
+            **Description:**\n\`\`\`${
+							command.help.description ||
+							"There is no Description for this command."
+						}\`\`\`\n**Usage:**\n\`\`\`${
+						command.help.usage || "No Usage"
+					}\`\`\`\n**Permissions:**\n\`\`\`${
+						command.help.accessableby || "Members"
+					}\`\`\`\n**Aliases:**\n\`\`\`${alia}\`\`\``
+				)
+				.setColor("#4a4b4d")
+				.setFooter(
+					`HAPPY | This command requested by ${message.author.username}#${message.author.discriminator}`
+				);
+
+			message.channel.send(embed);
+		} else {
+			var embeds = new Discord.MessageEmbed()
+				.setTitle(`**Command: ${helpArgs[0]}**`)
+				.setDescription(
+					`
+            **Response:**
+						\`\`\`Error: 404 Not Found\`\`\``
+				)
+				.setColor("#ff0000");
+
+			return message.channel.send(embeds);
+		}
+	}
+};
+
+module.exports.help = {
+	name: "help",
+	description: "This command is used for displaying all commands.",
+	usage: "d!help",
+	accessableby: "Members",
+	aliases: [],
+};
