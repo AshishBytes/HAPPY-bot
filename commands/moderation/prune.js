adebc476d92a0502157fe48f94648974eb4b73f1
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-04-07 11:10:14 +0530

    Update prune.js

--- commands/moderation/prune.js
+++ commands/moderation/prune.js
@@ -1,39 +1,41 @@
-const Discord = require('discord.js')
+//Copyright (c) 2019, Eric Cohen.
+module.exports.run = (client, msg, args) => {
+    const logger = client.logger;
+    console.log(args);
+    //Number of messages to delete
+    var temp = parseInt(args[0]) || 0;//parseInt(msg.content.substring(6).trim()) || 0;
+    console.log(temp);
+      //Discord has a limit of 100 messages.
+			if(temp < 1 || temp > 99)
+				msg.reply('Please select a number from 1-99');
+			else {
+          //Trying to figure out why not adding 1 deletes temp-1 but
+          //adding 1 deletes temp+1
+			    console.log(temp);
+          //Because 2 messages get deleted if only 1 is requested.
+          //if(temp == 1)
+            //temp--;
+            //Gets messages to be deleted.
+            //To Be Fixed at a Later Date.
+				msg.channel.fetchMessages({limit: (temp+1/*Because logic*/)}).then(messages => {
+				    const unpinnedMessages = messages.filter(msg => !(msg.pinned)); //A collection of messages that aren't pinned
+				    msg.channel.bulkDelete(unpinnedMessages, true);
+            //For case of 1 message to be deleted.
+            //if(temp == 0)
+            //  placeholder = 0;
+				    msgsDeleted = unpinnedMessages.array().length; // number of messages deleted
+            //Kind of wish I commented this when I originally coded it.
+            client.flag = true;
+					msg.channel.send(msgsDeleted-1 + ' message(s) deleted!');
+					logger(msgsDeleted-1 + ' message(s) deleted!');
+          //msg.delete();
+				}).catch(err => {
+					logger('Error while doing Bulk Delete', client.day);
+					logger(err, client.day);
+          msg.delete();
+				});
+			}
 
 
-module.exports.run = (message, args) => {
-        console.log("purging messages")
-
-        const amount = parseInt(args[0]) + 1;
-
-        if (isNaN(amount)) {
-            return message.reply('that doesn\'t seem to be a valid number.');
-        } else if (amount <= 1 || amount > 100) {
-            return message.reply('you need to input a number between 1 and 99.');
-        }
-
-        message.channel.bulkDelete(amount, true).then(deletedMessages => {
-            // Filter the deleted messages with .filter()
-            var botMessages = deletedMessages.filter(m => m.author.bot);
-            var userPins = deletedMessages.filter(m => m.pinned);
-            var userMessages = deletedMessages.filter(m => !m.author.bot);
-
-            const embed = new Discord.RichEmbed()
-                .setTitle("Success")
-                .setColor(0x00AE86)
-                .setFooter("HAPPY", "https://raw.githubusercontent.com/phantomdev-github/Resources/master/Discord%20Bots/Guardian/src/avatar.png")
-                .setThumbnail("https://raw.githubusercontent.com/phantomdev-github/Resources/master/Discord%20Bots/Guardian/src/avatar.png")
-                .setTimestamp()
-                .setURL("https://github.com/phantomdev-github/Resources/tree/master/Discord%20Bots/Guardian")
-                .addField("Bot Messages Purged", botMessages.size, false)
-                .addField("User Pins Purged", userPins.size, false)
-                .addField("User Messages Purged", userMessages.size, false)
-                .addField("Total Messages Purged", deletedMessages.size, false);
-
-            message.channel.send(embed);
-        }).catch(err => {
-            console.error(err);
-            message.channel.send('there was an error trying to prune messages in this channel!');
-        });
-    },
-};
+				//msg.delete(3000)
+}
