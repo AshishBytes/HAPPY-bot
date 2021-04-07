ca2cf4bb10d09d6f6cac198f489ec2ec9dfb70a3
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-04-07 10:14:16 +0530

    Update prune.js

--- commands/moderation/prune.js
+++ commands/moderation/prune.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args, ops) => {
   
 message.delete();
   
-const Error = new Discord.RichEmbed()
+const Error = new Discord.MessageEmbed()
 .setAuthor(`| ${client.user.tag}`, client.user.displayAvatarURL)
 .setDescription("You do not have the authorization to operate this command.")
 .setColor(ops.color)
@@ -16,7 +16,7 @@ if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send
 const user = message.mentions.users.first() || message.guild.members.get(args[0]);
 // Parse Amount
   
-const NoArgs = new Discord.RichEmbed()
+const NoArgs = new Discord.MessageEmbed()
 .setDescription("~prune [UserID] [#]")
 .setColor(ops.color)
 .setAuthor(`| ${client.user.tag}`, client.user.displayAvatarURL)
@@ -26,7 +26,7 @@ if (!user){
 message.channel.send(NoArgs).then(m => m.delete(5000));
 }
 
-const NoAmount = new Discord.RichEmbed()
+const NoAmount = new Discord.MessageEmbed()
 .setDescription(`Choose a number between \`1\` - \`100\` to clear from **${user.user.tag}** in ${message.channel}`)
 .setColor(ops.color)
 .setAuthor(`| ${client.user.tag}`, client.user.displayAvatarURL)
@@ -62,13 +62,13 @@ message.channel.fetchMessages({
  message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
 });
   
-const Done = new Discord.RichEmbed() //Creating the embed
+const Done = new Discord.MessageEmbed() //Creating the embed
 .setDescription(`\`${args[1]}\` messages have been pruned from **${user.user.tag}** in ${message.channel}`) //Setting the description
 .setColor(ops.color) //Setting the color
 .setAuthor(`| ${client.user.tag}`, client.user.displayAvatarURL) //Setting the title
 .setFooter("HAPPY") //Setting the footer
 
-const LogsMsg = new Discord.RichEmbed()
+const LogsMsg = new Discord.MessageEmbed()
 .setAuthor(`| ${client.user.tag }`, client.user.displayAvatarURL)
 .setDescription(`**User** ${user.user.tag}\n**Amount** ${args[1]}\n**Reason** ${reason}\n**Moderator** ${message.author.tag}\n**Channel** ${message.channel}\n**At** ${moment(message.createdAt).format("lll", Date.now())}`)
 .setColor(ops.color)
