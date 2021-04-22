ed375ba9dfdc24c60b7ccb36112b5e78a41898cf
HAPPY7131 <75130408+HAPPY7131@users.noreply.github.com>
2021-04-22 17:48:52 +0530

    Update music.js

--- music.js
+++ music.js
@@ -259,16 +259,6 @@ try{
         return
         return;
     }
-    else if (command === "ping") {
-        return embedbuilder(client, message, `AQUA`, `PING:`, `\`${client.ws.ping} ms\``)
-    }
-    else if (command === "uptime") {
-        let days = Math.floor(client.uptime / 86400000);
-        let hours = Math.floor(client.uptime / 3600000) % 24;
-        let minutes = Math.floor(client.uptime / 60000) % 60;
-        let seconds = Math.floor(client.uptime / 1000) % 60;
-        return embedbuilder(client, message, `AQUA`, `UPTIME:`, `\`${days}d\` \`${hours}h\` \`${minutes}m\` \`${seconds}s\n\``)
-    }
     else if (command === "play" || command === "p") {
         embedbuilder(client, message, "AQUA", "Searching!", args.join(" ")).then(msg => msg.delete({ timeout: 5000 }).catch(console.error))
         return distube.play(message, args.join(" "));
